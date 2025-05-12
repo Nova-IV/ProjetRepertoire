e1c75f732585db2f5d5425b41a7a189e5501b6d3
Nova-IV <sofiane.zaidi@outlook>
2025-05-12 15:52:53 +0200

    index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les routes</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <?php
+        include_once __DIR__ . './routes/route.php';
+    ?>
+
+    <main>
+        <?php include_once __DIR__ . './pages/$page.php'; ?>
+    </main>
+
+    <footer>
+        <p>&copy; - PHP - 2025</p>
+    </footer>
+    
+</body>
+</html>
